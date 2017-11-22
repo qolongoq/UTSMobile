03e272b851d1d575cfa280ed38d9d9805ab444a1
qolongoq <qolongoq@gmail.com>
2017-11-22 14:17:51 +0700

    Perbaikan tombol kembali detail java

--- app/src/main/java/com/example/arka/custom/DetailActivity.java
+++ app/src/main/java/com/example/arka/custom/DetailActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -56,5 +57,13 @@ public class DetailActivity extends AppCompatActivity {
         }catch (Exception ex){
 
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

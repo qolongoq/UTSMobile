41b5cbc0461b00c6145caf06ff1ca74d3adc3641
qolongoq <qolongoq@gmail.com>
2017-11-22 13:47:37 +0700

    Tombol back form hasil

--- app/src/main/java/com/example/arka/custom/HasilActivity.java
+++ app/src/main/java/com/example/arka/custom/HasilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.MenuItem;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -12,10 +13,20 @@ public class HasilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
 
         TextView th = (TextView) findViewById(R.id.totalHasil);
         th.setText(""+intent.getStringExtra("hasil"));
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

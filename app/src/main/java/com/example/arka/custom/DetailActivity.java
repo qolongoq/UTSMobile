5906b5b617919fe94c4c13f00947960b572c65e3
GalihJaya <galihputraa11@github.com>
2017-11-22 14:14:46 +0700

    Perubahan Custom java

--- app/src/main/java/com/example/arka/custom/DetailActivity.java
+++ app/src/main/java/com/example/arka/custom/DetailActivity.java
@@ -1,17 +1,15 @@
 package com.example.arka.custom;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
-    EditText panjang, lebar;
+    EditText sisi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +18,7 @@ public class DetailActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        panjang = (EditText) findViewById(R.id.panjang);
-        lebar = (EditText) findViewById(R.id.lebar);
-
-    }
-    public void tombolHitung(View view){
-        try{
-            Intent intent = new Intent(DetailActivity.this, HasilActivity.class);
-            int nilaiA = Integer.parseInt(panjang.getText().toString());
-            int nilaiB = Integer.parseInt(lebar.getText().toString());
-            int hasil = nilaiA * nilaiB;
-            intent.putExtra("hasil",""+hasil);
-            //Toast.makeText(this, ""+hasil, Toast.LENGTH_SHORT).show();
-            startActivity(intent);
-
-        }catch (Exception ex){
-
-        }
+        sisi = (EditText) findViewById(R.id.sisi);
 
     }
 
@@ -46,5 +28,33 @@ public class DetailActivity extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void tombolHitung1(View view) {
+        try{
+            Intent intent = new Intent(DetailActivity.this, HasilActivity.class);
+            int nilaiA = Integer.parseInt(sisi.getText().toString());
+            int hasil = nilaiA * nilaiA;
+            intent.putExtra("hasil",""+hasil);
+            //Toast.makeText(this, ""+hasil, Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+
+        }catch (Exception ex){
+
+        }
+    }
+
+    public void tombolHitung2(View view) {
+        try{
+            Intent intent = new Intent(DetailActivity.this, HasilActivity.class);
+            int nilaiA = Integer.parseInt(sisi.getText().toString());
+            int hasil = 4 * nilaiA;
+            intent.putExtra("hasil",""+hasil);
+            //Toast.makeText(this, ""+hasil, Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+
+        }catch (Exception ex){
+
+        }
     }
 }
